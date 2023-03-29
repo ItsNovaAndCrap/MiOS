284d4c736ddca4694739b62f8043160f8089582c
ItsNovaAndCrap <masonmarchbank@gmail.com>
2023-03-29 16:00:28 -0300

    Completed first batch of OS code.

---
+++ src/impl/x86_64/boot/main.asm
@@ -0,0 +1,127 @@
+global start
+extern long_mode_start
+
+section .text
+bits 32
+start:
+    mov esp, stack_top
+
+    call check_multiboot
+    call check_cpuid
+    call check_long_mode
+
+    call setup_page_tables
+    call enable_paging
+
+    lgdt [gdt64.pointer]
+    jmp gdt64.code_segment:long_mode_start
+    
+    hlt
+
+check_multiboot:
+    cmp eax, 0x36D76289
+    jne .NoMultiboot
+    ret
+.NoMultiboot:
+    mov al, 'M'
+    jmp error
+
+check_cpuid:
+    pushfd
+    pop eax
+    mov ecx, eax
+    xor eax, 1 << 21
+    push eax
+    popfd
+    pushfd
+    pop eax
+    push ecx
+    popfd
+    cmp eax, ecx
+    je .NoCpuId
+    ret
+.NoCpuId:
+    mov al, 'C'
+    jmp error
+
+check_long_mode:
+    mov eax, 0x80000000
+    cpuid
+    cmp eax, 0x80000001
+    jb .NoLongMode
+
+    mov eax, 0x80000001
+    cpuid
+    test edx, 1 << 29
+    jz .NoLongMode
+    ret
+.NoLongMode:
+    mov al, 'L'
+    jmp error
+
+setup_page_tables:
+    mov eax, page_table_l3
+    or eax, 0b11
+    mov [page_table_l4], eax
+
+    mov eax, page_table_l2
+    or eax, 0b11
+    mov [page_table_l3], eax
+
+    mov ecx, 0
+.Loop:
+    mov eax, 0x200000
+    mul ecx
+    or eax, 0b10000011
+    mov [page_table_l2 + ecx * 8], eax
+
+    inc ecx
+    cmp ecx, 512
+    jne .Loop
+    ret
+
+enable_paging:
+    mov eax, page_table_l4
+    mov cr3, eax
+
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+    ret
+
+error:
+    mov dword [0xB8000], 0x4F524F45
+    mov dword [0xB8004], 0x4F3A4F52
+    mov dword [0xB8008], 0x4F204F20
+    mov byte  [0xB800A], al
+    hlt
+
+section .bss
+align 4096
+page_table_l4:
+    resb 4096
+page_table_l3:
+    resb 4096
+page_table_l2:
+    resb 4096
+stack_bottom:
+    resb 4096 * 4
+stack_top:
+
+section .rodata
+gdt64:
+    dq 0
+.code_segment: equ $ - gdt64
+    dq (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53)
+.pointer:
+    dw $ - gdt64 - 1
+    dq gdt64
